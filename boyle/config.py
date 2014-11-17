f450f241040e325b20ad84474508b3ea3aa0b073
Alexandre M. S <alexsavio@gmail.com>
2014-11-17 18:11:48 +0100

    Add mhd extensions to config.py

--- boyle/config.py
+++ boyle/config.py
@@ -3,20 +3,20 @@ import os
 import logging
 from collections import OrderedDict
 
-#Known two-part file extensions
-ALLOWED_EXTS = {'.gz': {'.nii'}}
+# Known two-part file extensions
+ALLOWED_EXTS = {'.gz': {'.nii', '.raw', '.mhd'}}
 
 LOG_LEVEL = logging.INFO
 
-#Acceptable extensions for DICOM files
+# Acceptable extensions for DICOM files
 DICOM_FILE_EXTENSIONS = ['.IMA', '.DICOM', '.DCM']
 DICOM_FILE_EXTENSIONS.extend([x.lower() for x in DICOM_FILE_EXTENSIONS])
 OUTPUT_DICOM_EXTENSION = '.dcm'
 
-#MACUTO-DATA PATH
+# MACUTO-DATA PATH
 TESTDATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'macuto-data')
 
-#DICOM field weights for dicom.comparison
+# DICOM field weights for dicom.comparison
 DICOM_FIELD_WEIGHTS = OrderedDict([('PatientID', 1),
                                    ('PatientName', 1),
                                    ('PatientAddress', 0.5),
