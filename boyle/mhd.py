198ab7d72cc782d482e46f3680f8920694568c3a
Alexandre M. S <alexsavio@gmail.com>
2015-01-20 23:56:39 +0100

    mhd.py: Fix raw file 3D reshaping

--- boyle/mhd.py
+++ boyle/mhd.py
@@ -107,12 +107,12 @@ def load_raw_data_with_mhd(filename):
     arr = [int(i) for i in meta_dict['DimSize'].split()]
     volume = reduce(lambda x, y: x*y, arr[0:dim-1], 1)
 
-    pwd = op.dirname(filename)
-    raw_file = meta_dict['ElementDataFile']
+    pwd       = op.dirname(filename)
+    raw_file  = meta_dict['ElementDataFile']
     data_file = op.join(pwd, raw_file)
 
-    ndtype  = MHD_TO_NUMPY_TYPE[meta_dict['ElementType']]
-    arrtype = NDARRAY_TO_ARRAY_TYPE[ndtype]
+    ndtype    = MHD_TO_NUMPY_TYPE[meta_dict['ElementType']]
+    arrtype   = NDARRAY_TO_ARRAY_TYPE[ndtype]
 
     with open(data_file, 'rb') as fid:
         binvalues = array.array(arrtype)
@@ -124,8 +124,8 @@ def load_raw_data_with_mhd(filename):
     if dim >= 3:
         # Begin 3D fix
         dimensions = [int(i) for i in meta_dict['DimSize'].split()]
-        dimensions.reverse()
-        data = data.reshape(dimensions)
+        # dimensions.reverse() ??
+        data       = data.reshape(dimensions)
         # End 3D fix
 
     return (data, meta_dict)
