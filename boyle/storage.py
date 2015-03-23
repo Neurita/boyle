b4d5fd3348b7b74f3d8a77328c8e626c8463d3d8
Alexandre M. S <alexsavio@gmail.com>
2015-03-23 16:32:45 +0100

    storage.py: improve hdf5 storage method for iterable content

--- boyle/storage.py
+++ boyle/storage.py
@@ -135,6 +135,9 @@ def save_variables_to_hdf5(file_path, variables, mode='w', h5path='/'):
     It is recommended to use numpy arrays as objects.
     List or tuples of strings won't work, convert them into numpy.arrays before.
     """
+    if not isinstance(variables, dict):
+        raise ValueError('Expected argument variables to be a dict, got a {}.'.format(type(variables)))
+
     h5file  = h5py.File(file_path, mode=mode)
     h5group = h5file.require_group(h5path)
 
@@ -147,7 +150,19 @@ def save_variables_to_hdf5(file_path, variables, mode='w', h5path='/'):
                 dt   = h5py.special_dtype(vlen=str)
                 data = data.astype(dt)
 
-            h5group[vn] = data
+            if isinstance(data, dict):
+                for key in data:
+                    #h5group.create_dataset(str(key))
+                    #import ipdb
+                    #ipdb.set_trace()
+                    h5group[str(key)] = data[key]
+
+            elif isinstance(data, list):
+                for idx, item in enumerate(data):
+                    #h5group.create_dataset(str(idx))
+                    h5group[str(idx)] = item
+            else:
+                h5group[vn] = data
     except:
         log.exception('Error saving {0} in {1}'.format(vn, file_path))
         raise
