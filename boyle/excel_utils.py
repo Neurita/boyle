566b51df4e2c939a2cb792f5f998763117bda094
Alexandre Manhaes Savio <alexsavio@gmail.com>
2016-02-03 13:25:28 +0100

    fix col_values in excel_utils

--- boyle/excel_utils.py
+++ boyle/excel_utils.py
@@ -151,7 +151,10 @@ def col_values(df, col_name):
     """ Return a list of not null values from the `col_name` column of `df`."""
     _check_cols(df, [col_name])
 
-    return [nom.lower() for nom in df[pd.notnull(df)][col_name] if not pd.isnull(nom)]
+    if 'O' in df[col_name] or pd.np.issubdtype(df[col_name].dtype, str): # if the column is of strings
+        return [nom.lower() for nom in df[pd.notnull(df)][col_name] if not pd.isnull(nom)]
+    else:
+        return [nom for nom in df[pd.notnull(df)][col_name] if not pd.isnull(nom)]
 
 
 def duplicated_rows(df, col_name):
